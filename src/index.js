ba7fde8fc8d731c6569bee4f6a4f8d464277ff2d
Artur Labacheuski <Corvus.Art@gmail.com>
2020-10-25 19:14:37 +0300

    fix: solved bracket task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = Object.fromEntries(bracketsConfig);
+  let stack = [];
+  const arr = str.split('');
+
+  for (let bracket of arr) {
+    if (stack[0] === bracket) {
+      stack.shift();
+    } else if (brackets[bracket]) {
+      stack.unshift(brackets[bracket]);
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
 }
